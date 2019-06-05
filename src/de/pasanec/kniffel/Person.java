12f898fa781f500e056cd35ea482068f70c6c2b1
pasanec <immanuel@pasanec.de>
2019-06-05 08:44:23 +0200

    new file:   src/de/pasanec/kniffel/Person.java

---
+++ src/de/pasanec/kniffel/Person.java
@@ -0,0 +1,47 @@
+package de.pasanec.kniffel;
+
+import java.util.ArrayList;
+
+public class Person {
+	private String name;
+	private Block block;
+	
+	public String getName() {
+		return name;
+	}
+	private void setName(String name) {
+		this.name = name;
+	}
+	public Block getBlock() {
+		return block;
+	}
+	private void setBlock(Block block) {
+		this.block = block;
+	}
+	
+	public Person(String name) {
+		this.setName(name);
+		this.setBlock(new Block());
+	}
+	
+	// Hilfsfunktionen zum einfachen Zugang
+	
+	public String ausgabe() {
+		String out = name + "\r\n";
+		out += "==================\r\n";
+		out += this.getBlock().ausgabe();
+		
+		return out;
+	}
+	
+	public int eingabe(ArrayList<Wuerfel> aw, int a) {
+		return this.getBlock().eintragen(aw, a);
+	}
+	
+	public int punkte() {
+		return this.getBlock().rechneZusammen();
+	}
+	
+	
+	
+}
